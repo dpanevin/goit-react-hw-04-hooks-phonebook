6871ed64af7c947b32fd065c7a1f7326ed8ef9f3
Denis Panevin <d.panevin@gmail.com>
2021-08-13 15:29:13 +0300

    Переносит приложение на хуки

--- src/components/Phonebook/Filter/Filter.jsx
+++ src/components/Phonebook/Filter/Filter.jsx
@@ -1,29 +1,22 @@
-import { Component } from 'react';
 import { FormInput } from '../Phonebook.styled';
 
-export class Filter extends Component {
-  hendleChange = e => {
-    const onFilterChange = this.props.onFilterChange;
-
+export function Filter({ onFilterChange, filterValue }) {
+  function hendleChange(e) {
     onFilterChange(e.target.value);
-  };
-
-  render() {
-    const filterValue = this.props.filterValue;
-
-    return (
-      <label>
-        Поиск по списку контактов:
-        <FormInput
-          onChange={this.hendleChange}
-          value={filterValue}
-          type="text"
-          name="filter"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Фильтр"
-          required
-        />
-      </label>
-    );
   }
+
+  return (
+    <label>
+      Поиск по списку контактов:
+      <FormInput
+        onChange={hendleChange}
+        value={filterValue}
+        type="text"
+        name="filter"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Фильтр"
+        required
+      />
+    </label>
+  );
 }
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,35 +1,33 @@
 import { nanoid } from 'nanoid';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactsList } from './Contacts/Contacts';
 import { Filter } from './Filter/Filter';
 import { HeadTitle, Section } from './Phonebook.styled';
 
-export class Phonebook extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-
-  componentDidMount() {
+export function Phonebook() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+  // component did mount
+  useEffect(() => {
     const localContactsList = JSON.parse(localStorage.getItem('contactsList'));
 
     if (localContactsList) {
-      this.setState({ contacts: localContactsList });
+      setContacts(localContactsList);
     }
-  }
-
-  componentDidUpdate(prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contactsList', JSON.stringify(this.state.contacts));
+    console.log('sfsfsdff');
+  }, []);
+  //componentdidupdate
+  useEffect(() => {
+    if (contacts) {
+      localStorage.setItem('contactsList', JSON.stringify(contacts));
     }
-  }
+  }, [contacts]);
 
-  onSubmit = info => {
+  function onSubmit(info) {
     const contact = { ...info, id: nanoid() };
-    const contactList = this.state.contacts;
-    const contactNames = contactList.map(item => {
+    const contactNames = contacts.map(item => {
       return item.name;
     });
 
@@ -40,44 +38,32 @@ export class Phonebook extends Component {
       return;
     }
 
-    this.setState(() => {
-      return {
-        contacts: [...contactList, contact],
-      };
-    });
-  };
-
-  onFilterChange = value => {
-    this.setState(() => {
-      const normalizeValue = value.toLowerCase();
-      return {
-        filter: normalizeValue,
-      };
-    });
-  };
-
-  onDelete = id => {
-    const currentContacts = this.state.contacts.filter(item => item.id !== id);
-    this.setState({ contacts: currentContacts });
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-
-    return (
-      <Section>
-        <HeadTitle>Phonebook</HeadTitle>
-        <ContactForm onSubmit={this.onSubmit} />
-
-        <h2>Contacts</h2>
-        <Filter filterValue={filter} onFilterChange={this.onFilterChange} />
-        <ContactsList
-          contacts={contacts}
-          filterValue={filter}
-          onDelete={this.onDelete}
-        />
-        <Toaster position="top-right" />
-      </Section>
-    );
+    setContacts([...contacts, contact]);
   }
+
+  function onFilterChange(value) {
+    const normalizeValue = value.toLowerCase();
+    setFilter(normalizeValue);
+  }
+
+  function onDelete(id) {
+    const currentContacts = contacts.filter(item => item.id !== id);
+    setContacts(currentContacts);
+  }
+
+  return (
+    <Section>
+      <HeadTitle>Phonebook</HeadTitle>
+      <ContactForm onSubmit={onSubmit} />
+
+      <h2>Contacts</h2>
+      <Filter filterValue={filter} onFilterChange={onFilterChange} />
+      <ContactsList
+        contacts={contacts}
+        filterValue={filter}
+        onDelete={onDelete}
+      />
+      <Toaster position="top-right" />
+    </Section>
+  );
 }
